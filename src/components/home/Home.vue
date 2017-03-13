c4d2562abe9a20915d5113a9d6ac591a5117eb00
honor <honor_loyalty@126.com>
2017-03-13 14:13:20 +0800

    complete basic config

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,284 @@
 <template>
   <div class="page home">
-
+    <header class="header">
+      <div class="headerbar">
+        <a class="search">
+          <i class="icon-magnifier-1 search-icon"></i>
+        </a>
+        <p class="title">
+          <a  class="wrap">
+            <i class="icon-loc-hollow"></i>
+            <span class="position">
+              新街口(地铁站)
+            </span>
+            <i class="icon-arrow-right-thin"></i>
+          </a>
+        </p>
+      </div>
+      <img src="../../assets/imgs/bannertemp.jpg" alt="" class="banner">
+    </header>
+    <div class="container">
+      <ul class="foods">
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+        <li class="food">
+          <img src="../../assets/imgs/meal.png" alt="" class="food_img">
+          <p class="food_name">美食</p>
+        </li>
+      </ul>
+      <section class="merchants">
+        <h6 class="merchants_title">
+          附近商家
+        </h6>
+        <div class="merchant">
+          <div class="avator">
+            <img src="../../assets/imgs/mer_avator.jpeg" alt="" class="merchant_logo">
+          </div>
+          <div class="detail">
+            <div class="shop">
+              <p class="name">南京大排档(德基店)</p>
+              <span class="distance">352m</span>
+            </div>
+            <div class="mid">
+              <div class="stars">
+                <div class="star">
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score score_half"></i>
+                  <i class=""></i>
+                </div>
+                <span class="sales">月售<i class="number">1389</i>份</span>
+              </div>
+              <span class="time">30分钟</span>
+            </div>
+            <p class="price">
+              <button class="deliver_btn">
+                美团专送
+              </button>
+              起送价
+              <span class="money">￥20</span>
+              <span class="line">|</span>
+              配送
+              <span class="deliver">￥3</span>
+            </p>
+          </div>
+        </div>
+        <div class="merchant">
+          <div class="avator">
+            <span class="ava brand">品牌</span>
+            <img src="../../assets/imgs/mer_avator.jpeg" alt="" class="merchant_logo">
+          </div>
+          <div class="detail">
+            <div class="shop">
+              <p class="name">南京大排档(德基店)</p>
+              <span class="distance">352m</span>
+            </div>
+            <div class="mid">
+              <div class="stars">
+                <div class="star">
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score score_half"></i>
+                  <i class=""></i>
+                </div>
+                <span class="sales">月售<i class="number">1389</i>份</span>
+              </div>
+              <span class="time">30分钟</span>
+            </div>
+            <p class="price">
+              <button class="deliver_btn">
+                美团专送
+              </button>
+              起送价
+              <span class="money">￥20</span>
+              <span class="line">|</span>
+              配送
+              <span class="deliver">￥3</span>
+            </p>
+          </div>
+          <div class="cards">
+            <p class="card">
+              <img src="../../assets/imgs/ling.png.webp" alt="" class="card_img">
+              <span class="card_desc">
+              可领4～8元代金券
+            </span>
+            </p>
+          </div>
+        </div>
+        <div class="merchant">
+          <div class="avator">
+            <span class="ava new">新品</span>
+            <img src="../../assets/imgs/mer_avator.jpeg" alt="" class="merchant_logo">
+          </div>
+          <div class="detail">
+            <div class="shop">
+              <p class="name">南京大排档(德基店)</p>
+              <span class="distance">352m</span>
+            </div>
+            <div class="mid">
+              <div class="stars">
+                <div class="star">
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score score_half"></i>
+                  <i class=""></i>
+                </div>
+                <span class="sales">月售<i class="number">1389</i>份</span>
+              </div>
+              <span class="time">30分钟</span>
+            </div>
+            <p class="price">
+              起送价
+              <span class="money">￥20</span>
+              <span class="line">|</span>
+              配送
+              <span class="deliver">￥3</span>
+            </p>
+          </div>
+          <div class="cards">
+            <p class="card">
+              <img src="../../assets/imgs/ling.png.webp" alt="" class="card_img">
+              <span class="card_desc">
+              可领4～8元代金券
+            </span>
+            </p>
+            <p class="card">
+              <img src="../../assets/imgs/fan.png.webp" alt="" class="card_img">
+              <span class="card_desc">
+              实际支付50元返3元商家代金券
+            </span>
+            </p>
+          </div>
+        </div>
+        <div class="merchant">
+          <div class="avator">
+            <span class="ava brand">品牌</span>
+            <img src="../../assets/imgs/mer_avator.jpeg" alt="" class="merchant_logo">
+          </div>
+          <div class="detail">
+            <div class="shop">
+              <p class="name">南京大排档(德基店)</p>
+              <span class="distance">352m</span>
+            </div>
+            <div class="mid">
+              <div class="stars">
+                <div class="star">
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score score_half"></i>
+                  <i class=""></i>
+                </div>
+                <span class="sales">月售<i class="number">1389</i>份</span>
+              </div>
+              <span class="time">30分钟</span>
+            </div>
+            <p class="price">
+              <button class="deliver_btn">
+                美团专送
+              </button>
+              起送价
+              <span class="money">￥20</span>
+              <span class="line">|</span>
+              配送
+              <span class="deliver">￥3</span>
+            </p>
+          </div>
+        </div>
+        <div class="merchant">
+          <div class="avator">
+            <span class="ava brand">品牌</span>
+            <img src="../../assets/imgs/mer_avator.jpeg" alt="" class="merchant_logo">
+          </div>
+          <div class="detail">
+            <div class="shop">
+              <p class="name">南京大排档(德基店)</p>
+              <span class="distance">352m</span>
+            </div>
+            <div class="mid">
+              <div class="stars">
+                <div class="star">
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score"></i>
+                  <i class="score score_half"></i>
+                  <i class=""></i>
+                </div>
+                <span class="sales">月售<i class="number">1389</i>份</span>
+              </div>
+              <span class="time">30分钟</span>
+            </div>
+            <p class="price">
+              <button class="deliver_btn">
+                美团专送
+              </button>
+              起送价
+              <span class="money">￥20</span>
+              <span class="line">|</span>
+              配送
+              <span class="deliver">￥3</span>
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+    <section class="menus_bar">
+      <ul class="menus">
+        <li class="menu active">
+          <a class="menu_link">
+            <i class="ico-bowl-fill"></i>
+            <span class="menu_text">
+            首页
+          </span>
+          </a>
+        </li>
+        <li class="menu">
+          <a class="menu_link">
+            <i class="ico-paper"></i>
+            <span class="menu_text">
+            订单
+          </span>
+          </a>
+        </li>
+        <li class="menu">
+          <a class="menu_link">
+            <i class="ico-user"></i>
+            <span class="menu_text">
+            我的
+          </span>
+          </a>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="page login">
+    <heder class="header">
+      <span class="back_btn"> < </span>
+      <span class="register_btn">立即注册</span>
+      <h3 class="title">登录</h3>
+    </heder>
+    <div class="container">
+      <ul class="login_tab">
+        <li class="tab_item active">手机验证登录</li>
+        <li class="tab_item">美团账号登录</li>
+      </ul>
+      <section class="tab_content">
+        <div class="form">
+          <div class="input_group">
+            <img src="../../assets/imgs/icon_signin_phone.png" alt="" class="img_icon">
+            <input type="phone" placeholder="请输入手机号">
+            <button class="code_btn">获取验证码</button>
+          </div>
+          <div class="input_group">
+            <img src="../../assets/imgs/icon_phone_check_code.png" alt="" class="img_icon">
+            <input type="phone" placeholder="请输入手机验证码">
+          </div>
+          <div class="input_group phone_number">
+            <p class="tip">未注册过手机将自动注册为美团账户</p>
+          </div>
+        </div>
+        <div class="form">
+          <div class="input_group">
+            <img src="../../assets/imgs/icon_login_user.png" alt="" class="img_icon">
+            <input type="phone" placeholder="请输入手机号">
+          </div>
+          <div class="input_group">
+            <img src="../../assets/imgs/icon_login_password.png" alt="" class="img_icon">
+            <input type="phone" placeholder="请输入手机验证码">
+          </div>
 
+        </div>
+      </section>
+      <div class="btn_wrapper">
+        <button class="login_btn">
+          登录
+        </button>
+      </div>
+      <div class="forget">
+        <a href="" class="forget_pwd">忘记密码</a>
+      </div>
+      <div class="tips">
+        <span class="tip">说明:注册/登录说明您已同意</span>
+        <a href="" class="agreement">《美团用户协议》</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +59,6 @@ export default {
 }
 </script>
 
-<style rel="stylesheet">
-
+<style lang="less">
+  @import "../../assets/less/login.less";
 </style>
